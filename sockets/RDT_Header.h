28e18f66abb7be364f6443ff18d2b4ad646bcf8c
TREVAND174 <trevoranderson17@gmail.com>
2014-05-17 00:48:06 -0700

    fun in the sun

--- sockets/RDT_Header.h
+++ sockets/RDT_Header.h
@@ -1,16 +1,42 @@
 #pragma once
 #include <string>
 #include <time.h>
+typedef int T;
+
 class RDT_Header
 {
 public:
-	std::string toString();
+	std::string TtoString(T t)
+	{
+		std::string ret;
+		ret.resize(sizeof(T));
+		memcpy(&ret[0], &t, sizeof(T));
+		return ret;
+	}
+	std::string RDT_Header::toString()
+	{
+		std::string ret;
+		ret += TtoString(seqNum);
+		ret += TtoString(ackNum);
+		ret += TtoString(len);
+		ret += TtoString(fin);
+		ret += data;
+		return ret;
+	}
 	RDT_Header()
 	{}
-	RDT_Header(std::string datagram);
-	unsigned char seqNum;
-	unsigned char ackNum;
-	unsigned char len;
-	unsigned char fin;
+	RDT_Header::RDT_Header(std::string datagram)
+	{
+		int size = sizeof(T);
+		memcpy(&seqNum, &datagram[0], size);
+		memcpy(&ackNum, &datagram[size * 1], size);
+		memcpy(&len, &datagram[size * 2], size);
+		memcpy(&fin, &datagram[size * 3], size);
+		data = datagram.substr(size * 4, datagram.size() - size * 4);
+	}
+	T seqNum;
+	T ackNum;
+	T len;
+	T fin;
 	std::string data;
 };
